a8532dc8a7b278720335d12a175dc226fb1721b9
Surajhat <60094316+Surajhat@users.noreply.github.com>
2020-02-26 13:47:01 +0530

    here's the code dear

---
+++ Javascript.js
@@ -0,0 +1,5 @@
+function hello(){
+    document.querySelector("#alert").style.display = "block";
+    document.querySelector("#alert").innerHTML = "  " + document.querySelector("#name").value ;
+}
+alert ("My first web code in Sololearn")
